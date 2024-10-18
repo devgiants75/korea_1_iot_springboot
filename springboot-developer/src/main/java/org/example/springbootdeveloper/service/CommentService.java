998dcfeb121aef16343d5366c5a065f337ec5e1e
devgiants75 <devgiants75@naver.com>
2024-10-18 17:35:57 +0900

    20241018

--- springboot-developer/src/main/java/org/example/springbootdeveloper/service/CommentService.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/service/CommentService.java
@@ -1,4 +1,22 @@
 package org.example.springbootdeveloper.service;
 
+import org.example.springbootdeveloper.dto.request.CommentRequestDto;
+import org.example.springbootdeveloper.dto.response.CommentResponseDto;
+import org.example.springbootdeveloper.dto.response.ResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CommentService {
+    public ResponseDto<CommentResponseDto> createComment(CommentRequestDto dto) {
+        return null;
+    }
+
+    public ResponseDto<CommentResponseDto> getCommentsByPost(Long postId) {
+    }
+
+    public ResponseDto<CommentResponseDto> updateComment(Long commentId, String newContent) {
+    }
+
+    public ResponseDto<CommentResponseDto> deleteComment(Long commentId) {
+    }
 }

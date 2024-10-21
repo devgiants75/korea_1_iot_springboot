7338a018766cd213e39104845730c12eb8c21487
devgiants75 <devgiants75@naver.com>
2024-10-21 09:29:12 +0900

    20241021

--- springboot-developer/src/main/java/org/example/springbootdeveloper/service/CommentService.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/service/CommentService.java
@@ -3,6 +3,7 @@ package org.example.springbootdeveloper.service;
 import org.example.springbootdeveloper.dto.request.CommentRequestDto;
 import org.example.springbootdeveloper.dto.response.CommentResponseDto;
 import org.example.springbootdeveloper.dto.response.ResponseDto;
+import org.example.springbootdeveloper.entity.Comment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,5 +24,12 @@ public class CommentService {
 
     public ResponseDto<Void> deleteComment(Long commentId) {
         return null;
+    }
+
+    private CommentResponseDto convertToCommentResponseDto(Comment comment) {
+        return new CommentResponseDto(
+                comment.getId(), comment.getPost().getId(), comment.getContent()
+                , comment.getCommenter()
+        );
     }
 }

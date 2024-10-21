f7105d09cc576e120bb13ca65538cf2f62bac12a
devgiants75 <devgiants75@naver.com>
2024-10-21 09:26:53 +0900

    20241021

--- springboot-developer/src/main/java/org/example/springbootdeveloper/dto/response/PostResponseDto.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/dto/response/PostResponseDto.java
@@ -1,10 +1,12 @@
 package org.example.springbootdeveloper.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class PostResponseDto {
     private Long id;
     private String title;
@@ -13,4 +15,5 @@ public class PostResponseDto {
 
     // 해당 게시글의 댓글 리스트를 포함
     private List<CommentResponseDto> comments;
+
 }
--- springboot-developer/src/main/java/org/example/springbootdeveloper/service/PostService.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/service/PostService.java
@@ -1,16 +1,35 @@
 package org.example.springbootdeveloper.service;
 
 import org.example.springbootdeveloper.dto.request.PostRequestDto;
+import org.example.springbootdeveloper.dto.response.CommentResponseDto;
 import org.example.springbootdeveloper.dto.response.PostResponseDto;
 import org.example.springbootdeveloper.dto.response.ResponseDto;
+import org.example.springbootdeveloper.entity.Post;
+import org.example.springbootdeveloper.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
+    @Autowired
+    private PostRepository postRepository;
+
     public ResponseDto<PostResponseDto> createPost(PostRequestDto dto) {
-        return null;
+        try {
+            Post post = Post.builder()
+                    .title(dto.getTitle())
+                    .content(dto.getContent())
+                    .author(dto.getAuthor())
+                    .build();
+            postRepository.save(post);
+            return ResponseDto.setSuccess("게시글이 정상적으로 등록되었습니다.", convertToPostResponseDto(post));
+        } catch (Exception e) {
+            return ResponseDto.setFailed("게시글 등록 중 오류가 발생했습니다: " + e.getMessage());
+        }
+
     }
 
     public ResponseDto<List<PostResponseDto>> getAllPosts() {
@@ -28,5 +47,15 @@ public class PostService {
 
     public ResponseDto<Void> deletePost(Long postId) {
         return null;
+    }
+
+    private PostResponseDto convertToPostResponseDto(Post post) {
+        List<CommentResponseDto> commentDtos = post.getComments().stream()
+                .map(comment -> new CommentResponseDto(comment.getId(), post.getId(), comment.getContent(), comment.getCommenter()))
+                .collect(Collectors.toList());
+
+        return new PostResponseDto(
+                post.getId(), post.getTitle(), post.getContent(), post.getAuthor(), commentDtos
+        );
     }
 }

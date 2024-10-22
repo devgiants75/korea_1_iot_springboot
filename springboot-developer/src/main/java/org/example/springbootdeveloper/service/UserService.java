cc260db3bb33b71e0d41c2194bdf3cb68ac7af52
devgiants75 <devgiants75@naver.com>
2024-10-22 16:41:50 +0900

    20241022

--- springboot-developer/src/main/java/org/example/springbootdeveloper/service/UserService.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/service/UserService.java
@@ -20,7 +20,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
-public class UserService implements UserDetailsService {
+public class UserService {
     // 비즈니스 로직
     // Controller의 요청을 받아 필요한 데이터를 Repository를 통해 얻거나 전달하고
     // , 기능 구현 후 응답을 Controller에게 전달
@@ -84,26 +84,22 @@ public class UserService implements UserDetailsService {
             }
 
             // 사용자 인증을 처리(인증 실패 시 예외가 발생)
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(
-                            dto.getEmail(),
-                            dto.getPassword()
-                    )
-            );
+//            Authentication authentication = authenticationManager.authenticate(
+//                    new UsernamePasswordAuthenticationToken(
+//                            dto.getEmail(),
+//                            dto.getPassword()
+//                    )
+//            );
 
             // 인증 성공 후 JWT 토큰 생성
             String token = jwtProvider.generateJwtToken(dto.getEmail());
-
             return new UserLoginResponseDto(token);
 
         } catch (Exception e) {
-            return null;
+            e.printStackTrace();
+            throw new RuntimeException("Login Failed: " + e.getMessage());
         }
 
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 }

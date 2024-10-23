8780ece5483b81f4117d19b055e37272a99ccfba
devgiants75 <devgiants75@naver.com>
2024-10-23 10:37:25 +0900

    20241023

--- springboot-developer/src/main/java/org/example/springbootdeveloper/config/WebSecurityConfig.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,17 +1,20 @@
 package org.example.springbootdeveloper.config;
 
-import lombok.RequiredArgsConstructor;
 import org.example.springbootdeveloper.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;

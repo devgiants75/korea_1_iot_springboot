1691db4ab8f5e1f1d17f0d4ac34cfbde7172da51
devgiants75 <devgiants75@naver.com>
2024-11-01 14:45:22 +0900

    20241101

--- springboot-developer/src/main/java/org/example/springbootdeveloper/common/constant/ApiMappingPattern.java
+++ springboot-developer/src/main/java/org/example/springbootdeveloper/common/constant/ApiMappingPattern.java
@@ -12,5 +12,6 @@ public class ApiMappingPattern {
     public static final String BOOK = "/api/v1/books";
 
     public static final String MENU = "/api/v1/menus";
-//    public static final String GROUP = "/api/v1/group";
+
+    public static final String TODO = "/api/v1/todos";
 }
